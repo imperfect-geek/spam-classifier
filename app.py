4b92bd9148a43fe48d19591b805ed31eb5c97584
aditya <adichoudhary2222@gmail.com>
2021-11-24 19:48:15 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+from sklearn.feature_extraction.text import CountVectorizer
+import joblib
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    df= pd.read_csv('archive/spam.csv', encoding="latin-1")
+    df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+    # Features and Labels
+    df.rename(columns={'v1': 'class', 'v2': 'message'}, inplace=True)
+    df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+    X = df['message']
+
+    cv = CountVectorizer()
+    X = cv.fit_transform(X) # Fit the Data
+    
+    
+    NB_spam_model = open('NB_spam_model.pkl','rb')
+    clf = joblib.load(NB_spam_model)
+
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+    return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
